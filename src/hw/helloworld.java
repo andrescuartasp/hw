3ad708aa72153a5c12d8b6dc44f1d651cd7ffbd1
andres.cuartas <andres.cuartas@CPX-QB9GMLJFT3J.dir.svc.accenture.com>
2018-03-01 14:34:53 -0500

    Primer push

---
+++ src/hw/helloworld.java
@@ -0,0 +1,15 @@
+package hw;
+
+public class helloworld {
+
+	public helloworld() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.print("Hello world");
+		System.out.print("Hello world 2");
+
+	}
+
+}
